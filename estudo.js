38c46f75bb0fbfe9536471c417299667f3046671
amorimcarla <108940613+amorimcarla@users.noreply.github.com>
2022-10-05 08:22:20 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,4 +1,5 @@
 let tabuada = 2;
+let nome = "carla";
 
 function escreva (){
      documen.write("Tabuada do " + tabuada + "<br>")
@@ -14,3 +15,7 @@ function escreva (){
      document.write(tabuada + " x 10 = "+(tabuada*1)+"<br>");
      document.write("Feito por " + carla);
 }
+
+function minhatabuada (){
+     for(let j = 1; j<= 10; j++){
+     document.write(tabuada + " x " + i + " = " + (tabuada*i)+"<br>");    
